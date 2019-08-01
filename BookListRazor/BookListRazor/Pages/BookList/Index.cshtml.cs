bee21882e60421cd945daf83269e44cbeca12a1b
Miguel Aguila <mlaaguilamapua@gmail.com>
2019-08-01 12:41:35 -0700

    Created Book Crud Functionality

--- BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -2,16 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //on the get we only need to pass the property
+        //also does not need a post right now
+        //Bind property is useful when you are actually posting something so that it is retrieved and you don't have to get that using parameters
+        public IEnumerable<Book> Books { get; set; }
+
+
+        //Not an IActionResult because we are just assigning the books here
+        public async Task OnGet()
+        {
+            //Retrieve all books and assign it to Books variable
+            Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
